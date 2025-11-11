2d3634a879aa019d34812c4f3fffde688aff46cd
Casa <pinedaarenasy@gmail.com>
2025-11-11 23:36:53 +0100

    Añadido de metodos adestrador

--- src/conversorXML.java
+++ src/conversorXML.java
@@ -2,27 +2,50 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.File;
+import java.util.List;
 
 public class conversorXML {
 
-    public static void main(String[] args) {
+    public void conversorAdestrador(List<adestrador> listaAdestrador) {
 
-        adestrador ad = new adestrador();
-        ad.setNome("Bruck");
-        ad.setNacemento("01/02/2003");
 
         try {
-            JAXBContext context = JAXBContext.newInstance(adestrador.class);
+            File file = new File("E:\\DAM\\ad\\Hibernate2\\xml\\adestradores.xml");
+            AdestradorListWrapper alw = new AdestradorListWrapper(listaAdestrador);
+            JAXBContext context = JAXBContext.newInstance(AdestradorListWrapper.class, adestrador.class);
             Marshaller m = context.createMarshaller();
             m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-            System.out.println("---Marshal to Console---");
-            m.marshal(ad, System.out);
-            File file = new File("/home/dam/ad/Hibernate22/src/xml/adestrador.xml");
-            m.marshal(ad, file);
+
+            m.marshal(alw, System.out);
+            m.marshal(alw, file);
+
             System.out.println("\n Sucessfully marshaled to: " + file.getAbsolutePath());
         } catch (JAXBException e) {
             throw new RuntimeException(e);
         }
     }
 
+
+    public void conversorPokedex(List<pokedex> listaPokedex) {
+
+        File file = new File("E:\\DAM\\ad\\Hibernate2\\xml\\pokedex.xml");
+        PokedexListWrapper wrapper = new PokedexListWrapper(listaPokedex);
+        try {
+            JAXBContext context = JAXBContext.newInstance(PokedexListWrapper.class, pokedex.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            m.marshal(wrapper, System.out);
+            m.marshal(wrapper, file);
+
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
 }
+
+
